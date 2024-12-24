f980ec6878bee08ac5e2393c352c0cc8d7665dd7
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-12-24 18:19:24 +0300

    refactor tree node to use branch instead of node type

--- QuadTreeNode.gd
+++ QuadTreeNode.gd
@@ -2,7 +2,7 @@ class_name QuadTreeNode extends Object
 
 enum Type {
 	VOID,
-	NODE,
+	BRANCH,
 	LEAF,
 }
 
@@ -17,20 +17,29 @@ var data: ForceGraphNode
 var nodes: Array[QuadTreeNode] = []
 var type: Type = Type.VOID
 
-func set_data(value: ForceGraphNode) -> void:
+func set_leaf_data(value: ForceGraphNode) -> void:
 	data = value
 	type = Type.LEAF
 
-func create_nodes() -> void:
+func init_branch() -> void:
 	nodes.clear()
 	for quadrant in Quadrant.values() as Array[int]: nodes.append(QuadTreeNode.new())
-	type = Type.NODE
+	type = Type.BRANCH
+
+func set_node(quadrant: Quadrant, value: QuadTreeNode) -> void:
+	if not is_branch():
+		push_error('Cant set node: not a branch')
+		return
+	
+	nodes[quadrant] = value
 
 func set_nodes(value: Array[QuadTreeNode]) -> void:
 	nodes = value
-	type = Type.NODE
+	type = Type.BRANCH
 
 func is_void() -> bool: return type == Type.VOID
+func is_branch() -> bool: return type == Type.BRANCH
+func is_leaf() -> bool: return type == Type.LEAF
 
 func _to_string() -> String:
 	return "QuadTreeNode(%d)" % [type]
