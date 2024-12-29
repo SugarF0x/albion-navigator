6bef29ab02c21b36002f6272069dda89648206cc
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-12-30 00:06:13 +0300

    add fps counter

--- Home.gd
+++ Home.gd
@@ -1,1 +1,6 @@
 extends Node2D
+
+@onready var fps_label: Label = $CanvasLayer/Control/PanelContainer/MarginContainer/VBoxContainer/FPSLabel
+
+func _process(delta: float) -> void:
+	fps_label.text = "FPS: {fps}".format({ "fps": roundf((1 / delta) * 100.0) / 100.0 })
