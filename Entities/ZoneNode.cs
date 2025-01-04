c2f44d307c108b681a84913b87a31089c9a9161c
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-01-04 22:40:37 +0300

    add proper zone node

---
+++ Entities/ZoneNode.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using AlbionNavigator.Data;
+using Godot;
+
+namespace AlbionNavigator.Entities;
+
+[Tool]
+[GlobalClass]
+public partial class ZoneNode : ForceGraphNode
+{
+	[Export]
+	public Zone.ZoneType Type
+	{
+		get => _type;
+		set
+		{
+			_type = value;
+			QueueRedraw();
+		}
+	}
+
+	private Zone.ZoneType _type = Zone.ZoneType.StartingCity; 
+
+	public readonly Dictionary<Zone.ZoneType, Color> TypeToColorMap = new()
+	{
+		{ Zone.ZoneType.StartingCity, Colors.Purple },
+		{ Zone.ZoneType.City, Colors.Green },
+		{ Zone.ZoneType.SafeArea, Colors.Blue },
+		{ Zone.ZoneType.Yellow, Colors.Yellow },
+		{ Zone.ZoneType.Red, Colors.Red },
+		{ Zone.ZoneType.Black, Colors.Black },
+		{ Zone.ZoneType.Road, Colors.White }
+	};
+	
+	public override void _Draw()
+	{
+		DrawCircle(Vector2.Zero, 5.0f, Colors.White);
+		DrawCircle(Vector2.Zero, 4.0f, TypeToColorMap[Type]);
+	}
+}
