db6fcd1742aaeee25bccfb0431dd411682a6b6bb
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-01-06 22:36:24 +0300

    add some main controls

---
+++ Views/Controls/main.gd
@@ -0,0 +1,17 @@
+extends TabBar
+
+const DUMMY_LINK = preload("res://DummyLink.tscn")
+
+@onready var graph := get_tree().get_first_node_in_group("ForceGraph") as ForceDirectedGraph
+@onready var add_random_link_button: Button = $MarginContainer/VBoxContainer/AddRandomLinkButton
+@onready var reheat_button: Button = $MarginContainer/VBoxContainer/ReheatButton
+
+func _ready() -> void:
+	add_random_link_button.pressed.connect(add_random_link)
+	reheat_button.pressed.connect(graph.Reheat)
+
+func add_random_link() -> void:
+	var link := DUMMY_LINK.instantiate() as ForceGraphLink
+	link.Source = randi_range(410, 790)
+	link.Target = randi_range(410, 790)
+	graph.AddLink(link)
