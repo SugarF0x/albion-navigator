9c094c6a33892127e27de1f7bd617b0f979aa9d6
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-01-06 20:23:41 +0300

    restrict graph mouse controls to when mosue is in scope

--- Views/GraphCamera.gd
+++ Views/GraphCamera.gd
@@ -1,15 +1,23 @@
 extends Camera2D
 
+@onready var background_shader_rect: ColorRect = %BackgroundShaderRect
+
 @export var min_zoom := 0.5
 @export var max_zoom := 4.0 
 
 var is_dragging: bool = false
+var is_mouse_in_scope: bool = false
 
 func _ready() -> void:
 	var center := get_viewport_rect().size / 2;
 	position = center
+	
+	background_shader_rect.mouse_entered.connect(set_is_mouse_in_scope.bind(true))
+	background_shader_rect.mouse_exited.connect(set_is_mouse_in_scope.bind(false))
 
 func _input(event: InputEvent) -> void:
+	if not is_mouse_in_scope: return
+	
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT: is_dragging = event.is_pressed()
 		elif event.button_index == MOUSE_BUTTON_WHEEL_UP: adjust_zoom(true)
@@ -24,3 +32,7 @@ func adjust_zoom(out := false) -> void:
 	new_zoom.x = clamp(new_zoom.x, min_zoom, max_zoom)
 	new_zoom.y = clamp(new_zoom.y, min_zoom, max_zoom)
 	zoom = new_zoom
+
+func set_is_mouse_in_scope(value: bool) -> void: 
+	is_mouse_in_scope = value
+	if not value: is_dragging = false
