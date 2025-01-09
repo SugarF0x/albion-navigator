ed4872550b6f40542ca785e55e578f64a4f0c09c
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-01-09 14:09:24 +0300

    add take screenshot function on global keybind

--- Autoload/ScreenCapture.cs
+++ Autoload/ScreenCapture.cs
@@ -1,18 +1,26 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 using Godot;
 
 namespace AlbionNavigator.Autoload;
 
+[GlobalClass]
 public partial class ScreenCapture : Node
 {
     [DllImport("user32.dll")]
     private static extern int GetAsyncKeyState(int vKey);
+    [DllImport("user32.dll")]
+    private static extern int GetSystemMetrics(int nIndex);
     
     const int VK_CONTROL = 0x11;
     const int VK_S = 0x53;
     
     private bool DidJustPressBind;
     private bool DidReleaseBind = true;
+    
+    [Signal]
+    public delegate void ScreenCapturedEventHandler();
 
     public override void _Ready()
     {
@@ -22,10 +30,9 @@ public partial class ScreenCapture : Node
     public override void _Process(double delta)
     {
         CheckForBindPress();
-        if (DidJustPressBind)
-        {
-            GD.Print("Control + S pressed globally!");
-        }
+        if (!DidJustPressBind) return;
+        
+        TakeScreenshot();
     }
 
     private void CheckForBindPress()
@@ -36,5 +43,23 @@ public partial class ScreenCapture : Node
 
         DidJustPressBind = GetAsyncKeyState(VK_CONTROL) > 0 && GetAsyncKeyState(VK_S) > 0;
         if (DidJustPressBind) DidReleaseBind = false;
+    }
+
+    private void TakeScreenshot()
+    {
+        var screenWidth = GetSystemMetrics(0);
+        var screenHeight = GetSystemMetrics(1);
+
+        using (var bitmap = new System.Drawing.Bitmap(screenWidth, screenHeight))
+        {
+            using (var graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.CopyFromScreen(0, 0, 0, 0, new Size(screenWidth, screenHeight));
+            }
+
+            bitmap.Save("D:\\Godot\\_TEMP\\image.png", ImageFormat.Png);
+        }
+        
+        EmitSignal(SignalName.ScreenCaptured);
     }
 }
