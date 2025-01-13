ce41c5062d693c768e4d56df9ab59f788992f1f7
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-01-13 23:44:20 +0300

    extend c captures to track capture time too

--- Views/Controls/CartographyCaptures.gd
+++ Views/Controls/CartographyCaptures.gd
@@ -1,4 +1,4 @@
-extends VBoxContainer
+class_name CartographyCaptures extends VBoxContainer
 
 const SAMPLE_IMAGE = preload("res://Assets/sample-image.png")
 
@@ -8,7 +8,10 @@ const SAMPLE_IMAGE = preload("res://Assets/sample-image.png")
 @onready var image_inspector: ImageInspector = $ImageInspector
 
 var _captures: Array[Texture2D] = []
+var _capture_stamps: Array[String] = []
 var _inspector_index := -1
+
+signal preview_image_changed(index: int, image: Texture2D, timestamp: String)
 
 func _ready() -> void:
 	previous_button.pressed.connect(func() -> void: _inspector_index = maxi(_inspector_index - 1, 0); _sync_inspector())
@@ -18,18 +21,29 @@ func _ready() -> void:
 
 func add_image(image: Texture2D) -> void:
 	_captures.append(image)
+	_capture_stamps.append(Time.get_datetime_string_from_system())
 	_sync_inspector()
 
 func pop_current_image() -> void:
 	if _inspector_index < 0 or _inspector_index > _captures.size() - 1: return
 	_captures.remove_at(_inspector_index)
+	_capture_stamps.remove_at(_inspector_index)
 	_sync_inspector()
+
+func get_current_expiration() -> String:
+	return _capture_stamps[_inspector_index] if _capture_stamps.size() - 1 >= _inspector_index and _inspector_index >= 0 else ""
 
 func _sync_inspector() -> void:
 	var total_captures := _captures.size()
+	
 	_inspector_index = mini(_inspector_index, total_captures - 1)
 	if _inspector_index < 0 and total_captures > 0: _inspector_index = 0
+	
 	next_button.disabled = _inspector_index + 1 == total_captures
 	previous_button.disabled = _inspector_index <= 0
+	
 	index_label.text = "-" if _inspector_index < 0 else str(_inspector_index + 1)
-	image_inspector.texture = SAMPLE_IMAGE if _captures.size() <= 0 else _captures[_inspector_index]
+	
+	image_inspector.texture = SAMPLE_IMAGE if total_captures <= 0 else _captures[_inspector_index]
+	
+	preview_image_changed.emit(_inspector_index, image_inspector.texture, get_current_expiration())
