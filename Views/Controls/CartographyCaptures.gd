eb325aa2d922c2059d0d1fe0b8294223d681ed03
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-01-15 12:49:04 +0300

    add pop current capture button

--- Views/Controls/CartographyCaptures.gd
+++ Views/Controls/CartographyCaptures.gd
@@ -7,6 +7,7 @@ const SAMPLE_IMAGE = preload("res://Assets/sample-image.png")
 @onready var next_button: Button = $HBoxContainer/NextButton
 @onready var image_inspector_1: ImageInspector = $HBoxContainer2/ImageInspector1
 @onready var image_inspector_2: ImageInspector = $HBoxContainer2/ImageInspector2
+@onready var clear_button: Button = $HBoxContainer/ClearButton
 
 var _captures: Array[Texture2D] = []
 var _capture_stamps: Array[String] = []
@@ -17,6 +18,7 @@ signal preview_image_changed(index: int, image: Texture2D, timestamp: String)
 func _ready() -> void:
 	previous_button.pressed.connect(func() -> void: _inspector_index = maxi(_inspector_index - 1, 0); _sync_inspector())
 	next_button.pressed.connect(func() -> void: _inspector_index = mini(_inspector_index + 1, _captures.size()); _sync_inspector())
+	clear_button.pressed.connect(pop_current_image)
 	ScreenCapture.ScreenCaptured.connect(add_image)
 	_sync_inspector()
 
@@ -37,6 +39,8 @@ func get_current_expiration() -> String:
 func _sync_inspector() -> void:
 	var total_captures := _captures.size()
 	
+	clear_button.disabled = total_captures == 0
+	
 	_inspector_index = mini(_inspector_index, total_captures - 1)
 	if _inspector_index < 0 and total_captures > 0: _inspector_index = 0
 	
@@ -48,5 +52,11 @@ func _sync_inspector() -> void:
 	var texture := SAMPLE_IMAGE if total_captures <= 0 else _captures[_inspector_index]
 	image_inspector_1.texture = texture
 	image_inspector_2.texture = texture
+	
+	if total_captures == 0:
+		image_inspector_1.texture_rect.scale = Vector2.ONE
+		image_inspector_1.adjust_scale()
+		image_inspector_2.texture_rect.scale = Vector2.ONE
+		image_inspector_2.adjust_scale()
 	
 	preview_image_changed.emit(_inspector_index, texture, get_current_expiration())
