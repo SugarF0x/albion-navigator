a29100be6867a547360f388381029976f5a9b7f5
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-03-07 14:10:41 +0100

    add unavailability overlay to screen captures on non-windows systems

--- Views/Controls/CartographyCaptures.gd
+++ Views/Controls/CartographyCaptures.gd
@@ -2,12 +2,15 @@ class_name CartographyCaptures extends VBoxContainer
 
 const SAMPLE_IMAGE = preload("res://Assets/sample-image.png")
 
-@onready var previous_button: Button = $HBoxContainer/PreviousButton
-@onready var index_label: Label = $HBoxContainer/IndexLabel
-@onready var next_button: Button = $HBoxContainer/NextButton
-@onready var image_inspector_1: ImageInspector = $HBoxContainer2/ImageInspector1
-@onready var image_inspector_2: ImageInspector = $HBoxContainer2/ImageInspector2
-@onready var clear_button: Button = $HBoxContainer/ClearButton
+@onready var previous_button: Button = %PreviousButton
+@onready var index_label: Label = %IndexLabel
+@onready var next_button: Button = %NextButton
+@onready var clear_button: Button = %ClearButton
+@onready var image_inspector_1: ImageInspector = %ImageInspector1
+@onready var image_inspector_2: ImageInspector = %ImageInspector2
+@onready var controls_container: HBoxContainer = %ControlsContainer
+@onready var captures_container: HBoxContainer = %CapturesContainer
+@onready var unavailable_overlay: Panel = %UnavailableOverlay
 
 var _captures: Array[Texture2D] = []
 var _capture_stamps: Array[String] = []
@@ -16,11 +19,18 @@ var _inspector_index := -1
 signal preview_image_changed(index: int, image: Texture2D, timestamp: String)
 
 func _ready() -> void:
+	set_os_visibility()
 	previous_button.pressed.connect(func() -> void: _inspector_index = maxi(_inspector_index - 1, 0); _sync_inspector())
 	next_button.pressed.connect(func() -> void: _inspector_index = mini(_inspector_index + 1, _captures.size()); _sync_inspector())
 	clear_button.pressed.connect(func() -> void: pop_current_image(); AudioPlayer.Play("PaperTrash"))
 	ScreenCapture.ScreenCaptured.connect(add_image)
 	_sync_inspector()
+
+func set_os_visibility() -> void:
+	if OS.get_name() == "Windows": return
+	controls_container.visible = false
+	captures_container.visible = false
+	unavailable_overlay.visible = true
 
 func add_image(image: Texture2D) -> void:
 	_captures.append(image)
