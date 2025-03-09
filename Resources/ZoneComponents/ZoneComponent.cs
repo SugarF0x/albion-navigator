477552f06b737411641fb13828501026f208094d
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-03-09 17:19:40 +0100

    add resource component resources

---
+++ Resources/ZoneComponents/ZoneComponent.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+
+[GlobalClass]
+public partial class ZoneComponent : Resource
+{
+    public enum ZoneComponentType
+    {
+        Dungeon,
+        Chest,
+        Gather,
+        MistsCity,
+    }
+
+    public enum ZoneComponentProperty
+    {
+        Small,
+        Big,
+        Keeper,
+        Heretic,
+        Morgana,
+        Undead,
+        Avalonian,
+        Green,
+        Blue,
+        Gold,
+        Ore,
+        Stone,
+        Wood,
+        Fiber,
+        Hide,
+    }
+    
+    [Export] public int Id { get; set; }
+    [Export] public ZoneComponentType Type { get; set; }
+    [Export] public string DisplayName { get; set; }
+    [Export] public int Tier { get; set; }
+    [Export] public Godot.Collections.Array<ZoneComponentProperty> Properties { get; set; } = [];
+}
