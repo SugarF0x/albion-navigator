b808d2605e504fa01fea3173900059eee1b89a41
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-03-09 17:20:01 +0100

    add main tab flush button TODO

--- Views/Controls/main.gd
+++ Views/Controls/main.gd
@@ -6,7 +6,10 @@ const LinkScene = preload("res://Entities/ZoneLink.tscn")
 
 @onready var reheat_button: Button = $MarginContainer/VBoxContainer/ReheatButton
 @onready var add_sample_data_button: Button = $MarginContainer/VBoxContainer/AddSampleDataButton
+@onready var flush_data_button: Button = $MarginContainer/VBoxContainer/FlushDataButton
 
 func _ready() -> void:
 	reheat_button.pressed.connect(graph.Reheat)
 	add_sample_data_button.pressed.connect(PersistedStore.LoadSampleData)
+	# TODO: remove all links that have expiry
+	# flush_data_button.pressed.connect()
