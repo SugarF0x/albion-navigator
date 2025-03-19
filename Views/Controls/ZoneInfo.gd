8bf4cb6a7fe94e18626c33cda1254f682682f7f1
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-03-19 22:17:32 +0100

    add zone components display to zone info tab

--- Views/Controls/ZoneInfo.gd
+++ Views/Controls/ZoneInfo.gd
@@ -7,14 +7,16 @@ extends TabBar
 @onready var layer_label: Label = %LayerLabel
 @onready var display_name_label: Label = %DisplayNameLabel
 @onready var connections_label: Label = %ConnectionsLabel
-@onready var components_container: VBoxContainer = %ComponentsContainer
+@onready var components_container: HBoxContainer = %ComponentsContainer
 
-var zone_group := preload('res://Resources/ZoneGroup.tres')
+const ZONE_COMPONENT_VIEW = preload("res://Components/ZoneComponentView.tscn")
+const ZONE_GROUP := preload('res://Resources/ZoneGroup.tres')
+
 var zones: Array[Zone] = []
 var zone_names: Array[String] = []
 
 func _ready() -> void:
-	for zone in zone_group.load_all():
+	for zone in ZONE_GROUP.load_all():
 		zones.append(zone as Zone)
 		zone_names.append(zone.DisplayName)
 	
@@ -35,3 +37,9 @@ func show_zone_details(zone_name: String) -> void:
 	layer_label.text = "Layer: {layer}".format({ "layer": zone.Layer })
 	display_name_label.text = "Display Name: {display_name}".format({ "display_name": zone.DisplayName })
 	connections_label.text = "Connections: {connections}".format({ "connections": ", ".join(zone.Connections) })
+	
+	for child in components_container.get_children(): child.queue_free()
+	for component: ZoneComponent in zone.Components:
+		var component_view := ZONE_COMPONENT_VIEW.instantiate() as ZoneComponentView
+		components_container.add_child(component_view)
+		component_view.component = component
