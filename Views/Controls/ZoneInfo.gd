2b28a1e833bccdb67d18a448845393636307c77c
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-03-21 14:41:14 +0100

    add zoom into inspected zones

--- Views/Controls/ZoneInfo.gd
+++ Views/Controls/ZoneInfo.gd
@@ -1,6 +1,8 @@
 extends TabBar
 
 @onready var graph := get_tree().get_first_node_in_group("ForceGraph") as ZoneMap
+@onready var camera := get_tree().get_first_node_in_group("GraphCamera") as GraphCamera
+
 @onready var zone_line: AutoCompleteLineEdit = %ZoneLine
 @onready var search_button: Button = %SearchButton
 @onready var id_label: Label = %IdLabel
@@ -9,6 +11,7 @@ extends TabBar
 @onready var display_name_label: Label = %DisplayNameLabel
 @onready var connections_label: Label = %ConnectionsLabel
 @onready var zone_component_stack: HFlowContainer = %ZoneComponentStack
+@onready var zoom_button: Button = %ZoomButton
 
 const ZONE_COMPONENT_VIEW = preload("res://Components/ZoneComponentView.tscn")
 const ZONE_GROUP := preload('res://Resources/ZoneGroup.tres')
@@ -50,6 +53,7 @@ func _ready() -> void:
 	
 	zone_line.options = zone_names
 	search_button.pressed.connect(on_search)
+	zoom_button.pressed.connect(zoom_on_current)
 	if graph: graph.PortalRegistered.connect(auto_inspect_added_portal)
 
 func _input(_event: InputEvent) -> void:
@@ -65,7 +69,11 @@ func on_search() -> void:
 	var search_value := zone_line.get_value()
 	if search_value.is_empty(): return
 	
-	show_zone_details_by_name(search_value)
+	var zone_id := get_zone_id_by_name(search_value)
+	if zone_id < 0: return
+	
+	show_zone_details(zone_id)
+	zoom_into_node(zone_id)
 	
 	zone_line.text = ""
 	zone_line.text_changed.emit("")
@@ -74,14 +82,22 @@ func on_search() -> void:
 	# hacky solution to update tab size but it works (disgusting)
 	(get_parent_control() as TabContainer).tab_changed.emit(-1)
 
-func show_zone_details_by_name(zone_name: String) -> void:
-	var zone_index := zone_names.find(zone_name)
-	show_zone_details(zone_index)
+func zoom_into_node(id: int) -> void:
+	var position := zones[id].Position + graph.position
+	camera.transition_to_position(position, 8.0)
 
+func zoom_on_current() -> void:
+	if inspected_id < 0: return
+	zoom_into_node(inspected_id)
+
+func get_zone_id_by_name(zone_name: String) -> int: return zone_names.find(zone_name)
+
+var inspected_id: int = -1
 func show_zone_details(zone_index: int) -> void:
 	if zone_index < 0: return
 	
 	var zone := zones[zone_index]
+	inspected_id = zone.Id
 	id_label.text = "Id: {id}".format({ "id": zone.Id })
 	type_label.text = "Type: {type}".format({ "type": ZoneType.keys()[zone.Type] })
 	layer_label.text = "Layer: {layer}".format({ "layer": ZoneLayer.keys()[zone.Layer] })
