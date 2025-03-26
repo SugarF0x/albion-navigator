e7e014cbe255ccbfe1b54b2cd89c6d0b8eadcc59
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-03-26 23:29:01 +0100

    hide some buttons from main tab based on build debug flag

--- Views/Controls/main.gd
+++ Views/Controls/main.gd
@@ -4,12 +4,16 @@ const LinkScene = preload("res://Entities/ZoneLink.tscn")
 
 @onready var graph := get_tree().get_first_node_in_group("ForceGraph") as ForceDirectedGraph
 
-@onready var reheat_button: Button = $MarginContainer/VBoxContainer/ReheatButton
-@onready var add_sample_data_button: Button = $MarginContainer/VBoxContainer/AddSampleDataButton
-@onready var flush_data_button: Button = $MarginContainer/VBoxContainer/FlushDataButton
+@onready var reheat_button: Button = %ReheatButton
+@onready var add_sample_data_button: Button = %AddSampleDataButton
+@onready var flush_data_button: Button = %FlushDataButton
 
 func _ready() -> void:
 	reheat_button.pressed.connect(graph.Reheat)
 	add_sample_data_button.pressed.connect(PersistedStore.LoadSampleData)
+	
+	add_sample_data_button.visible = OS.is_debug_build()
+	flush_data_button.visible = OS.is_debug_build()
+	
 	# TODO: remove all links that have expiry
 	# flush_data_button.pressed.connect()
