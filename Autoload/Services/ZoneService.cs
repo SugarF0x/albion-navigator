0f3deccd8d0b38f96f62bdd7417a6aa91ef1f42d
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-04-14 23:26:17 +0200

    remove tasks from zone service

--- Autoload/Services/ZoneService.cs
+++ Autoload/Services/ZoneService.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Godot;
 using GodotResourceGroups;
 
@@ -9,13 +8,13 @@ namespace AlbionNavigator.Autoload.Services;
 
 public class ZoneService
 {
-    // TODO: go back from Tasked instnace to static one and rely on IsReady flag
-    
-    private static Task<ZoneService> _instance;
-    public static Task<ZoneService> Instance {
+    private static ZoneService _instance;
+    public static ZoneService Instance {
         set => ArgumentNullException.ThrowIfNull(value);
-        get => _instance ??= Init();
+        get => _instance ??= new ZoneService();
     }
+
+    public bool IsReady;
 
     private ResourceGroupBackgroundLoader _loader;
     public delegate void ResourceLoadedHandler(ResourceGroupBackgroundLoader.ResourceLoadingInfo loadingInfo);
@@ -27,32 +26,30 @@ public class ZoneService
     
     public Zone[] Zones;
 
-    private static async Task<ZoneService> Init() 
+    private ZoneService()
     {
-        var instance = new ZoneService();
-        await instance.LoadZones();
-        return instance;
+        LoadZones();
     }
 
-    private Task LoadZones()
+    private void LoadZones()
     {
         var zoneGroup = ResourceGroup.Of("res://Resources/ZoneGroup.tres");
         _loader = zoneGroup.LoadAllInBackground(EmitResourceLoaded);
 
-        var tcs = new TaskCompletionSource();
         List<Resource> zoneList = [];
-
         ResourceLoaded += Handler;
-        return tcs.Task;
+        
+        return;
 
         void Handler(ResourceGroupBackgroundLoader.ResourceLoadingInfo info)
         {
             zoneList.Add(info.Resource);
             if (!info.Last) return;
-
+            
             Zones = zoneList.Cast<Zone>().ToArray();
             Array.Sort(Zones, (a, b) => a.Id - b.Id);
             
+            IsReady = true;
             ResourceLoaded -= Handler;
             AllResourcesLoaded?.Invoke();
         }
