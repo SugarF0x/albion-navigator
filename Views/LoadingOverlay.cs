41427ad22dd8a53da4e5e598920155293001dbcc
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-04-20 16:56:08 +0200

    procedurally populate map with nodes and links

--- Views/LoadingOverlay.cs
+++ Views/LoadingOverlay.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Linq;
 using System.Threading.Tasks;
 using AlbionNavigator.Entities;
@@ -14,6 +15,7 @@ public partial class LoadingOverlay : CanvasLayer
     private Label _progressLabel;
 
     private Action _updateLabels;
+    private float _lastProcessTime;
     
     public override void _Ready()
     {
@@ -23,6 +25,7 @@ public partial class LoadingOverlay : CanvasLayer
 
     public override void _Process(double delta)
     {
+        if (_lastProcessTime == 0f) _lastProcessTime = (float)delta;
         _updateLabels?.Invoke();
     }
 
@@ -35,7 +38,7 @@ public partial class LoadingOverlay : CanvasLayer
     
     private async void LoadServices()
     {
-        Func<Task>[] serviceLoaders = [LoadZones, LoadLinks];
+        Func<Task>[] serviceLoaders = [LoadZones, PopulateMap];
 
         for (int i = 0; i < serviceLoaders.Length; i++)
         {
@@ -78,27 +81,88 @@ public partial class LoadingOverlay : CanvasLayer
         }
     }
 
-    private Task LoadLinks()
+    private Task PopulateMap()
     {
-        _ = LinkService.Instance;
-        return Task.CompletedTask;
+        var lastLoadedZoneInfo = new LastLoadedZoneInfo();
+        var queue = new Queue(ZoneService.Instance.Zones);
+        
+        var tcs = new TaskCompletionSource();
+
+        _updateLabels = () =>
+        {
+            _progressLabel.Text = $"Added Zone: {lastLoadedZoneInfo.Name}";
+            _minorProgressBar.Value = lastLoadedZoneInfo.Progress;
+        };
+        
+        ProcessQueue();
+        
+        return tcs.Task;
+
+        async void ProcessQueue()
+        {
+            try
+            {
+                var initialReheatValue = ZoneMap.Instance.ReheatOnNodesAdded;
+                ZoneMap.Instance.ReheatOnNodesAdded = false;
+                
+                while (queue.Count > 0)
+                {
+                    var frameStart = Time.GetTicksUsec();
+
+                    while (queue.Count > 0)
+                    {
+                        var zone = (Zone)queue.Dequeue();
+                        if (zone == null) continue;
+                        
+                        ZoneMap.Instance.InstantiateNode(zone);
+                        lastLoadedZoneInfo.Name = zone.DisplayName;
+                        lastLoadedZoneInfo.Progress = (float)(ZoneService.Instance.Zones.Length - queue.Count) / ZoneService.Instance.Zones.Length;
+                        
+                        var elapsed = Time.GetTicksUsec() - frameStart;
+                        if (!(elapsed / 1000.0 >= _lastProcessTime)) continue;
+                        
+                        await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
+                        break;
+                    }
+                }
+                
+                ZoneMap.Instance.ReheatOnNodesAdded = initialReheatValue;
+                OnAllZonesInserted();
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+        
+        void OnAllZonesInserted()
+        {
+            _updateLabels = null;
+            _minorProgressBar.Value = 1f;
+            tcs.SetResult();
+        }
     }
 
     private void OnAllServicesLoaded()
     {
         if (ZoneMap.Instance == null) throw new ArgumentNullException(nameof(ZoneMap.Instance));
         
-        _progressLabel.Text = "Populating map... (app might freeze for a second or two)";
-        _minorProgressBar.Value = 0.0;
+        _progressLabel.Text = "Starting simulation...";
+        _majorProgressBar.Value = 1.0;
+        _minorProgressBar.Value = 1.0;
 
         ToSignal(GetTree(), "process_frame").OnCompleted(() => CallDeferred("StartSimulation"));
     }
 
     private void StartSimulation()
     {
-        ZoneMap.Instance.PopulateZones();
         ZoneMap.Instance.Reheat(0f);
-        
         QueueFree();
+    }
+
+    private class LastLoadedZoneInfo()
+    {
+        public string Name;
+        public float Progress;
     }
 }
