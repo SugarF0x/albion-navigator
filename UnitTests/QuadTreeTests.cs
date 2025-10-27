27bd7333b1d1ea162cb9b7e08f893382007a2a59
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-10-27 23:02:37 +0100

    add unit tests project

---
+++ UnitTests/QuadTreeTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+using AlbionNavigator.Utils.ForceDirectedGraph.Internal.QuadTree;
+
+namespace UnitTests;
+
+[TestClass]
+public sealed class QuadTreeTests
+{
+    // public static IEnumerable<object[]> Cases =>
+    // [
+    //     [Vector2.One, new Data(0, "someValue")],
+    // ];
+    //
+    // [DataTestMethod]
+    // [DynamicData(nameof(Cases))]
+    // public void AddTests(Vector2 point, Data data)
+
+    [TestMethod]
+    public void AddTests()
+    {
+        var tree = new QuadTree<Data>();
+        tree.Add(Vector2.One, new Data(0, "e"));
+        
+        Assert.IsTrue(tree.IsLeaf);
+    }
+}
+
+public record Data(int Index, string Value);
