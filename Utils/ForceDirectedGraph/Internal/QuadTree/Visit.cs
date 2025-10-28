ba98afa2f1542ee435f21f119f6523fbf8301e9b
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-10-28 11:32:21 +0100

    add Visit method

---
+++ Utils/ForceDirectedGraph/Internal/QuadTree/Visit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AlbionNavigator.Utils.ForceDirectedGraph.Internal.QuadTree;
+
+public partial class QuadTree<T>
+{
+    public delegate bool VisitCallback(QuadTree<T> tree);
+    
+    public QuadTree<T> Visit(VisitCallback callback)
+    {
+        List<QuadTree<T>> trees = [];
+        if (!Root.IsVoid) trees.Add(Root);
+        
+        while (true)
+        {
+            if (trees.Count == 0) break;
+            var tree = trees.Last();
+            trees.RemoveAt(trees.Count - 1);
+            
+            if (tree.IsVoid) continue;
+            if (callback(tree)) continue;
+
+            for (var childQuadrantIndex = 3; childQuadrantIndex >= 0; childQuadrantIndex--)
+            {
+                var child = tree.Children?[childQuadrantIndex];
+                if (child == null) continue;
+                trees.Add(child);
+            }
+        }
+
+        return Root;
+    }
+}
