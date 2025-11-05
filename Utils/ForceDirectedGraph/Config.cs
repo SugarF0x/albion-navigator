e42b11c296c97af9bcb0de7beebc35d17baef509
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-11-05 10:40:13 +0100

    sync alphadecay with alphamin

--- Utils/ForceDirectedGraph/Config.cs
+++ Utils/ForceDirectedGraph/Config.cs
@@ -8,11 +8,21 @@ public partial class Simulation
     public float InitialAngle = (float)Math.PI * (3 - float.Sqrt(5));
     
     public float Alpha = 1;
-    public float AlphaMin = 0.001f;
     public float AlphaTarget;
 
     public float AlphaDecay = 0.02276277904418933f;
     public float VelocityDecay = 0.6f;
+    
+    private float _alphaMin = 0.001f;
+    public float AlphaMin
+    {
+        get => _alphaMin;
+        set
+        {
+            _alphaMin = value;
+            AdjustAlphaDecayToAlphaMin();
+        }
+    }
 
     public void AdjustAlphaDecayToAlphaMin()
     {
