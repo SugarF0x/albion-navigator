e49e4c76820b1e24611e1dd5b8b51c6adceab449
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-11-05 20:56:32 +0100

    add debug button to add small portal path

--- Views/Home/ControlPanel/SettingsTab/SettingsTab.cs
+++ Views/Home/ControlPanel/SettingsTab/SettingsTab.cs
@@ -10,6 +10,7 @@ public partial class SettingsTab : MarginContainer
 	private Button LoadSampleLinksButton;
 	private Button AddOneSampleLinkButton;
 	private Button FlushStorageButton;
+	private Button AddShortPortalPathButton;
 
 	public override void _Ready()
 	{
@@ -17,10 +18,16 @@ public partial class SettingsTab : MarginContainer
 		LoadSampleLinksButton = GetNode<Button>("%LoadSampleLinksButton");
 		AddOneSampleLinkButton = GetNode<Button>("%AddOneSampleLinkButton");
 		FlushStorageButton = GetNode<Button>("%FlushStorageButton");
+		AddShortPortalPathButton = GetNode<Button>("%AddShortPortalPathButton");
 
 		LoadSampleLinksButton.Pressed += () => LinkService.Instance.LoadSampleLinks();;
 		AddOneSampleLinkButton.Pressed += () => LinkService.Instance.AddLink(new ZoneLink(0, 450, DateTimeOffset.UtcNow.AddSeconds(3).ToString("O")));
 		FlushStorageButton.Pressed += () => LinkService.Instance.FlushStorage();
+		AddShortPortalPathButton.Pressed += () => LinkService.Instance.AddLink([
+			new ZoneLink(0, 450, DateTimeOffset.UtcNow.AddSeconds(3).ToString("O")),
+			new ZoneLink(450, 580, DateTimeOffset.UtcNow.AddSeconds(3).ToString("O")),
+			new ZoneLink(580, 630, DateTimeOffset.UtcNow.AddSeconds(3).ToString("O")),
+		]);
 		
 		if (!OS.HasFeature("debug")) DebugContainer.QueueFree();
 	}
