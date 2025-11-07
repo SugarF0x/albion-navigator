6ddb491842d0e4b3b36f3e37a521ccb8ad3abc36
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-11-07 20:47:02 +0100

    add shortest pathfinding

--- Views/Home/ControlPanel/NavigationTab/FindShortestPath.cs
+++ Views/Home/ControlPanel/NavigationTab/FindShortestPath.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using AlbionNavigator.Components.Navigation;
 using AlbionNavigator.Services;
 using Godot;
 
@@ -5,16 +7,25 @@ namespace AlbionNavigator.Views.Home.ControlPanel.NavigationTab;
 
 public partial class FindShortestPath : FoldableContainer
 {
-	public OptionButton FromOptionButton;
-	public OptionButton ToOptionButton;
+	private OptionButton FromOptionButton;
+	private OptionButton ToOptionButton;
+	private Button SearchShortestPathButton;
+	private PathList PathList;
 	
 	public override void _Ready()
 	{
 		FromOptionButton = GetNode<OptionButton>("%FromOptionButton"); 
 		ToOptionButton = GetNode<OptionButton>("%ToOptionButton");
+		SearchShortestPathButton = GetNode<Button>("%SearchShortestPathButton");
+		PathList = GetNode<PathList>("%PathList");
 
 		if (ZoneService.Instance.IsReady) AssignOptions();
 		else ZoneService.Instance.AllResourcesLoaded += AssignOptions;
+
+		SearchShortestPathButton.Pressed += SearchShortestPath;
+		NavigationService.Instance.ShortestPathUpdated += SyncPathList;
+
+		SyncPathList([]);
 	}
 
 	private void AssignOptions()
@@ -24,5 +35,17 @@ public partial class FindShortestPath : FoldableContainer
 			FromOptionButton.AddItem(zone.DisplayName, zone.Id);
 			ToOptionButton.AddItem(zone.DisplayName, zone.Id);
 		}
+	}
+
+	private void SearchShortestPath()
+	{
+		if (FromOptionButton.Selected < 0 || ToOptionButton.Selected < 0) return;
+		NavigationService.Instance.FindShortestPath(FromOptionButton.Selected, ToOptionButton.Selected);
+	}
+
+	private void SyncPathList(int[] path)
+	{
+		PathList.Zones = path.Select(index => ZoneService.Instance.Zones[index]).ToArray();
+		PathList.Visible = PathList.Zones.Length > 0;
 	}
 }
