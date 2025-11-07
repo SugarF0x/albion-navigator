a2bae8663ac912d74841e27433013d1d7ab113f1
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-11-07 20:23:50 +0100

    move find shortest path into ites own scene

--- Views/Home/ControlPanel/NavigationTab/NavigationTab.cs
+++ Views/Home/ControlPanel/NavigationTab/NavigationTab.cs
@@ -1,28 +1,8 @@
-using AlbionNavigator.Services;
 using Godot;
 
 namespace AlbionNavigator.Views.Home.ControlPanel.NavigationTab;
 
 public partial class NavigationTab : MarginContainer
 {
-	public OptionButton FromOptionButton;
-	public OptionButton ToOptionButton;
 	
-	public override void _Ready()
-	{
-		FromOptionButton = GetNode<OptionButton>("%FromOptionButton"); 
-		ToOptionButton = GetNode<OptionButton>("%ToOptionButton");
-
-		if (ZoneService.Instance.IsReady) AssignOptions();
-		else ZoneService.Instance.AllResourcesLoaded += AssignOptions;
-	}
-
-	public void AssignOptions()
-	{
-		foreach (var zone in ZoneService.Instance.Zones)
-		{
-			FromOptionButton.AddItem(zone.DisplayName, zone.Id);
-			ToOptionButton.AddItem(zone.DisplayName, zone.Id);
-		}
-	}
 }
