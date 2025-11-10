be424fb8affac0c6326614c2d46c18f99cd0de1d
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-11-10 23:02:14 +0100

    allow changing icon to text map

--- Views/Home/ControlPanel/SettingsTab/SettingsTab.cs
+++ Views/Home/ControlPanel/SettingsTab/SettingsTab.cs
@@ -1,9 +1,12 @@
 using System;
-using AlbionNavigator.Autoload;
+using System.Collections.Generic;
+using AlbionNavigator.Resources;
 using AlbionNavigator.Services;
 using Godot;
 
 namespace AlbionNavigator.Views.Home.ControlPanel.SettingsTab;
+
+// TODO: split dropdowns into their own scenes
 
 public partial class SettingsTab : MarginContainer
 {
@@ -13,6 +16,8 @@ public partial class SettingsTab : MarginContainer
 	private Button FlushStorageButton;
 	private Button AddShortPortalPathButton;
 	private HSlider VolumeSlider;
+	private Label ZoneTypeSampleLabel;
+	private TextEdit ZoneTypeSampleEdit;
 
 	public override void _Ready()
 	{
@@ -22,6 +27,8 @@ public partial class SettingsTab : MarginContainer
 		FlushStorageButton = GetNode<Button>("%FlushStorageButton");
 		AddShortPortalPathButton = GetNode<Button>("%AddShortPortalPathButton");
 		VolumeSlider = GetNode<HSlider>("%VolumeSlider");
+		ZoneTypeSampleLabel = GetNode<Label>("%ZoneTypeDisplayNameLabel");
+		ZoneTypeSampleEdit = GetNode<TextEdit>("%ZoneTypeIconTextEdit");
 
 		LoadSampleLinksButton.Pressed += () => LinkService.Instance.LoadSampleLinks();;
 		AddOneSampleLinkButton.Pressed += () => LinkService.Instance.AddLink(new ZoneLink(0, 450, DateTimeOffset.UtcNow.AddSeconds(3).ToString("O")));
@@ -36,5 +43,39 @@ public partial class SettingsTab : MarginContainer
 		VolumeSlider.ValueChanged += value => { SettingsService.Instance.Volume.Value = (float)value; };
 		
 		if (!OS.HasFeature("debug")) DebugContainer.QueueFree();
+
+		SetupZoneToIconMap();
+	}
+
+	private void SetupZoneToIconMap()
+	{
+		var container = ZoneTypeSampleLabel.GetParent();
+		container.RemoveChild(ZoneTypeSampleLabel);
+		container.RemoveChild(ZoneTypeSampleEdit);
+
+		foreach (var type in Enum.GetValues(typeof(Zone.ZoneType)))
+		{
+			var label = ZoneTypeSampleLabel.Duplicate() as Label;
+			var edit = ZoneTypeSampleEdit.Duplicate() as TextEdit;
+			if (label == null || edit == null)
+			{
+				GD.PrintErr("label or edit are not of proper node types");
+				return;
+			}
+			
+			container.AddChild(label);
+			container.AddChild(edit);
+
+			label.Text = type.ToString();
+			edit.Text = SettingsService.Instance.ZoneTypeToChatIconCode.Value[(Zone.ZoneType)type];
+			edit.TextChanged += () =>
+			{
+				SettingsService.Instance.ZoneTypeToChatIconCode.Value =
+					new Dictionary<Zone.ZoneType, string>(SettingsService.Instance.ZoneTypeToChatIconCode.Value)
+					{
+						[(Zone.ZoneType)type] = edit.Text
+					};
+			};
+		}
 	}
 }
